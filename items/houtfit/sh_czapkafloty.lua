f6ed104f2f479022a1000cee48bfd418d2182862
Helios <vakkendwarf@gmail.com>
2020-09-21 02:59:32 +0200

    initial commit

---
+++ items/houtfit/sh_czapkafloty.lua
@@ -0,0 +1,19 @@
+ITEM.name = "Marynarka: Czapka Marynarza"
+ITEM.description = "Czapka pasująca do munduru marzynarza floty imperialnej. Pasuje tylko do męskiej wersji munduru."
+ITEM.model = "models/niksacokica/tech/tech_library_datapad_stone.mdl"
+ITEM.outfitCategory = "czapka"
+ITEM.noResetBodyGroups = true
+
+ITEM.bodyGroups = {
+	["Hat/Helmet"] = 1
+}
+
+ITEM.height = 1
+ITEM.width = 1
+ITEM.category = "Marynarka"
+ITEM.classes = {CLASS_FLOTAZALOGANT, CLASS_FLOTAADMIRAL}
+
+ITEM.allowedModels = {
+	"models/player/bunny/zephyr_fleet/male/male_fleet_one.mdl",
+	"models/player/bunny/zephyr_fleet/male/male_fleet_two.mdl"
+}
---
+++ sh_plugin.lua
@@ -0,0 +1,4 @@
+local PLUGIN = PLUGIN
+PLUGIN.name = "Helios Outfits"
+PLUGIN.author = "Helios"
+PLUGIN.desc = "Fixes some issues with Helix's builtin outfit system."
